935f16b1e5e487d227e7692b90e1d74c2d2de13a
Chris-Petty <sirchristoffee@gmail.com>
2015-08-11 23:34:49 +1200

    refactored functional_tests.py

--- function_tests.py
+++ function_tests.py
@@ -13,6 +13,11 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Chris hears about a cool new online to-do app. He goes
         # to check out its homepage
@@ -36,10 +41,7 @@ class NewVisitorTest(unittest.TestCase):
         # When he hits enter, the page updates, and now the page lists
         # "1: go to dentist" as a item in a to-do list
         inputbox.send_keys(Keys.ENTER)
-
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: go to dentist', [row.text for row in rows])
+        self.check_for_row_in_list_table('1: go to dentist')
 
         # There is still a text box inviting him to add another item.
         # He enters, "finish TDD tutorial and praise our goat overlord"
@@ -48,12 +50,9 @@ class NewVisitorTest(unittest.TestCase):
         inputbox.send_keys(Keys.ENTER)
 
         # The page updates again and now shows both items on his list
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn(
-            '2: finish TDD tutorial and praise our goat overlord',
-            [row.text for row in rows]
-        )
+        self.check_for_row_in_list_table('1: go to dentist')
+        self.check_for_row_in_list_table('2: finish TDD tutorial and praise our goat overlord')
+
 
         # Chris wonders whether the site will remember his list. He then
         # that the site has generated a unique URL for him -- there is
